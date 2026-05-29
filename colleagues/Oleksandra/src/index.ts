bcc6d6ff0a940885316daaaaf46b33401afeddad
xyh <xyh@xyh.pl>
2026-05-29 16:57:14 +0200

    Clean up2

--- colleagues/Oleksandra/src/index.ts
+++
@@ -1,6 +0,0 @@
-export { WorkflowEngine } from './engine/WorkflowEngine';
-export { Visit } from './models/Visit';
-export { Task } from './models/Task';
-export { AuditEvent } from './models/AuditEvent';
-export * from './types/enums';
-export * from './types/interfaces';
